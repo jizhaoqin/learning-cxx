1cb5dc978264dfed5df8cc25d6c9bbd76eb7f472
jizhaoqin <jizhao.qin.de@gmail.com>
2025-01-15 01:31:49 +0100

    finish: 24-32

--- exercises/27_strides/main.cpp
+++ exercises/27_strides/main.cpp
@@ -1,4 +1,7 @@
 ﻿#include "../exercise.h"
+#include <algorithm>
+#include <cstddef>
+#include <iostream>
 #include <vector>
 
 // 张量即多维数组。连续存储张量即逻辑结构与存储结构一致的张量。
@@ -18,6 +21,12 @@ std::vector<udim> strides(std::vector<udim> const &shape) {
     // TODO: 完成函数体，根据张量形状计算张量连续存储时的步长。
     // READ: 逆向迭代器 std::vector::rbegin <https://zh.cppreference.com/w/cpp/container/vector/rbegin>
     //       使用逆向迭代器可能可以简化代码
+    udim stride = 1;
+    for (size_t i = 0; i < shape.size(); i += 1) {
+        strides[i] = stride;
+        stride *= *(shape.rbegin() + i);
+    }
+    std::reverse(strides.begin(), strides.end());
     return strides;
 }
 
